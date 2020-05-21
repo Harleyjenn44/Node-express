ef582ac792df047b0a5ab6e4f4d16c44c98cee15
Ashton <ashtonbeaudoin2000@hotmail.com>
2020-05-20 19:08:20 -0500

    updated server page

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-var express = require("express");
+const express = require("express");
+var exphbs = require("express-handlebars");
+const routes = require("./modals/burger");
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
